f4b798c1dd5c1668f65e19511f3adb45c5c59049
Nhan918 <truyennhan2910@gmail.com>
2025-09-06 17:28:08 +0700

    Revert "fixup! fs/namespace.c: Fix -Wunused-variable"
    This reverts commit c32f6ab61450bae8078fc238f272bdf58d52c07a.

--- fs/namespace.c
+++ fs/namespace.c
@@ -101,6 +101,10 @@ enum {
 	UMOUNT_STATUS_MAX
 };
 
+static const char *umount_exit_str[UMOUNT_STATUS_MAX] = {
+	"ADDED_TASK", "REMAIN_NS", "REMAIN_CNT", "DELAY_TASK"
+};
+
 static const char *exception_process[] = {
 	"main", "ch_zygote", "usap32", "usap64", NULL,
 };
